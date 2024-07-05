33641e6f35023e47042d6d570231c362687b793a
Prashant Kumar <prashantnawa123@gmail.com>
2024-07-05 17:20:12 +0530

    Revert "final responsive UI"
    This reverts commit e3c731898dd90d10b0eabad9a1578ab242cdec03.

--- script.js
+++ script.js
@@ -1,53 +1,36 @@
-// Get all nav links
-const navLinks = document.querySelectorAll('.nav-link');
+var swiper = new Swiper(".swiper-container", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-navLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
 
-    const sectionId = link.getAttribute('href').replace('#', '');
+  document.addEventListener("DOMContentLoaded", function () {
+    const faqItems = document.querySelectorAll(".faq-item");
 
-    const section = document.getElementById(sectionId);
-
-    section.scrollIntoView({ behavior: 'smooth' });
-    navLinks.forEach((otherLink) => {
-      otherLink.classList.remove('active');
+    faqItems.forEach(item => {
+        item.querySelector(".faq-question").addEventListener("click", () => {
+            item.classList.toggle("active");
+            const answer = item.querySelector(".faq-answer");
+            const icon = item.querySelector(".toggle-icon");
+            if (item.classList.contains("active")) {
+                answer.style.display = "block";
+                icon.textContent = "-";
+            } else {
+                answer.style.display = "none";
+                icon.textContent = "+";
+            }
+        });
     });
-
-    link.classList.add('active');
-  });
-});
-
-// Get all question cards
-const questionCards = document.querySelectorAll('.question-card');
-
-questionCards.forEach((questionCard) => {
-  const question = questionCard.querySelector('.question');
-  const answer = questionCard.querySelector('.answer');
-  const plusIcon = question.querySelector('.plus-icon');
-  const minusIcon = question.querySelector('.minus-icon');
-
-  question.addEventListener('click', () => {
-    console.log("triger")
-    if (answer.classList.contains('active')) {
-      answer.classList.remove('active');
-      plusIcon.style.display = 'block';
-      minusIcon.style.display = 'none';
-    } else {
-      answer.classList.add('active');
-      plusIcon.style.display = 'none';
-      minusIcon.style.display = 'block';
-    }
-  });
-});
-
-window.addEventListener('scroll', () => {
-  const navHeader = document.querySelector('.header-nav');
-  const scrollPosition = window.scrollY;
-
-  if (scrollPosition > 0) {
-    navHeader.classList.add('animatedbg');
-  } else {
-    navHeader.classList.remove('animatedbg');
-  }
 });
